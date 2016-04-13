7501296bb82b1d6258ca31e2bdf0468d8ad05fca
BJ Yurkovich <bjyurkovich@BJs-MacBook-Air.local>
2016-04-13 19:29:09 -0400

    latest

--- dates.php
+++ dates.php
@@ -5,22 +5,21 @@ require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
 ?>
 
 <div class="dates-container">
-<ul>
-<?php 
-	//Print out all the deadlines from the config file
-	$dates = $json['dates'];
-	for ($i = 0; $i < count($dates); $i++) {
-    echo '<li>
-			<div class="title">'.$dates[$i]['title'].'</div>
-			<div class="date"><i class="fa fa-lg fa-calendar" aria-hidden="true"></i> 
-				'.$dates[$i]['date'].'</div>
-		</li>';
-} 
-
-?>
-</ul>
+	<ul>
+		<?php 
+			//Print out all the deadlines from the config file
+			$dates = $json['dates'];
+			for ($i = 0; $i < count($dates); $i++) {
+		    echo '<li>
+					<div class="title">'.$dates[$i]['title'].'</div>
+					<div class="date"><i class="fa fa-lg fa-calendar" aria-hidden="true"></i> 
+						'.$dates[$i]['date'].'</div>
+				</li>';
+		} 
+		?>
+	</ul>
 
 </div>
 
 
-<?php require 'right-bar-footer.php'; //THE MUST BE INCLUDED FOR EACH PAGE ?>
+<?php require 'right-bar-footer.php'; //THIS MUST BE INCLUDED FOR EACH PAGE ?>
