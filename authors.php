8675ff7a1872f0f18bcad4db7554d4655350a269
BJ Yurkovich <bjyurkovich@BJs-MacBook-Air.local>
2016-04-13 11:51:13 -0400

    updates

--- authors.php
+++ authors.php
@@ -5,7 +5,27 @@ require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
 ?>
 
 <div class="text">
-	This is where the author info goes.
+	<h1>Submission Types</h1>
+	<p><div class="callout">Call for Contributed Papers:</div> Papers are invited in the form of regular manuscripts. Papers must conform to the submission policy, described below, requiring that all manuscripts be in 2-column format and meet strict page limits. </p>
+
+
+	<p><div class="callout">Call for Invited Sessions:</div> Invited sessions consist of 5-6 papers presenting a unifying theme from a diversity of viewpoints. Proposals must clearly describe the motivation and relevance of the session. Proposals must be accompanied by full versions of each paper, which will be individually reviewed together with the proposal itself. Individual papers may be removed from a proposed session and replaced by appropriate contributed papers. In case an entire proposed session is rejected, selected papers may be accepted as contributed ones. </p>
+
+
+	<p><div class="callout">Call for Tutorial Sessions:</div> Tutorial sessions and panel discussions addressing state-of-the-art control theory and advanced industrial applications are solicited.</p>
+
+	<p><div class="callout">Call for Workshops and Tutorials:</div> Workshops and tutorials to be held prior to the conference are solicited on all related topics. Proposals for workshops addressing novel control methodologies and nonstandard control applications are strongly encouraged.</p>
+
+	<div class="spacer"></div>
+	<h1>Submission Specifics</h1>
+
+	All papers and session proposals must be submitted through the conference submission website css.paperplaza.net and must be submitted in English. All submissions must conform to the following policy: 
+	<ul>
+		<li>All submitted papers must be formatted in the standard 2-column Proceedings format.</li>
+		<li>For the purposes of REVIEW, regular and invited papers are limited to 8 pages. Papers exceeding these limits cannot be uploaded.</li>
+		<li>A regular registration by an Author will allow the Author to upload up to 3 of the accepted papers the Author has (co)authored, and a reduced registration will allow the Student/Retiree Author to upload up to one of the accepted papers the (Student/Retiree) Author has (co)authored.</li>
+	</ul>
+
 </div>
 
 
--- organizers.php
+++ organizers.php
@@ -4,9 +4,125 @@ $stylesheet='css/organizers.css';
 require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
 ?>
 
-<div class="text">
+<!-- <div class="text">
 	This is where the organizers go.
-</div>
+</div> -->
 
+<div class="flex-column">
+	<div class="organizer-group-title">Honorary Chair</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/astrom.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Karl J. Astrom</div>
+			<!-- <div class="role bold">Honorary Chair</div> -->
+			<div class="affiliation">Lund University</div>
+		</div>
+	</div>
+	
+	<div class="spacer"></div>
+	
+	<div class="organizer-group-title">Advisory Board</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/samad.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Tariq Samad</div>
+			<div class="role">2009 IEEE CSS President (CSS)</div>
+			<div class="affiliation">Honeywell</div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/yamamoto.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Yutaka Yamamoto</div>
+			<div class="role">2013 IEEE CSS President (SICE & CSS)</div>
+			<div class="affiliation">Prof. Emeritus, Kyoto Univ</div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/siciliano.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Bruno Siciliano</div>
+			<div class="role">2008-09 IEEE RAS President (RAS)</div>
+			<div class="affiliation">University of Naples Federico II</div>
+		</div>
+	</div>
+
+	<div class="spacer"></div>
+	
+	<div class="organizer-group-title">Organizing Committee</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/yurkovich.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Stephen Yurkovich</div>
+			<div class="role bold">General Chair</div>
+			<div class="affiliation">University of Texas at Dallas</div>
+			<div class="contact"><a href="mailto:steve.yurkovich@utdallas.edu">steve.yurkovich@utdallas.edu</a></div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/spong.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Mark  W. Spong</div>
+			<div class="role bold">Program Co-Chair</div>
+			<div class="affiliation">University of Texas at Dallas</div>
+			<div class="contact"><a href="mailto:mspong@utdallas.edu">mspong@utdallas.edu</a></div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/rotea.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Mario Rotea</div>
+			<div class="role bold">Program Co-Chair</div>
+			<div class="affiliation">University of Texas at Dallas</div>
+			<div class="contact"><a href="mailto:rotea@utdallas.edu">rotea@utdallas.edu</a></div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/yen.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Gary Yen</div>
+			<div class="role bold">Events Chair</div>
+			<div class="affiliation">Oklahoma State University</div>
+			<div class="contact"><a href="mailto:gyen@okstate.edu">gyen@okstate.edu</a></div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/gonzalez.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Oscar Gonzalez</div>
+			<div class="role bold">Publications Chair </div>
+			<div class="affiliation">Old Dominion</div>
+			<div class="contact"><a href="mailto:ogonzale@odu.edu">ogonzale@odu.edu</a></div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/onori.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Simona Onori</div>
+			<div class="role bold">Registration Chair</div>
+			<div class="affiliation">Clemson University</div>
+			<div class="contact"><a href="mailto:sonori@clemson.edu">sonori@clemson.edu</a></div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/barbieri.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Enrique Barbieri</div>
+			<div class="role bold">Finance Chair</div>
+			<div class="affiliation">University of North Texas</div>
+			<div class="contact"><a href="mailto:enrique.barbieri@unt.edu">enrique.barbieri@unt.edu</a></div>
+		</div>
+	</div>
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/gregg.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Robert Gregg</div>
+			<div class="role bold">Workshops Chair</div>
+			<div class="affiliation">University of Texas at Dallas</div>
+			<div class="contact"><a href="mailto:rgregg@utdallas.edu">rgregg@utdallas.edu</a></div>
+		</div>
+	</div>
+
+</div>
 
 <?php require 'right-bar-footer.php'; //THE MUST BE INCLUDED FOR EACH PAGE ?>
