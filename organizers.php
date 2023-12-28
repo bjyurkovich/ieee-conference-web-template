36c6d03e158b477d3690d5616125e8946e661241
Zihan Zhou <786187157@qq.com>
2023-12-28 15:30:30 +0800

    update

--- organizers.php
+++ organizers.php
@@ -24,20 +24,20 @@ require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
 	<div class="organizer-group-title">TPC Co-Chairs</div>
 
 	<div class="flex flex-auto person-container">
-		<div class="pic headshot round" style="background:url(images/organizers/chonggangwang.png);background-size:100px 100px;"></div>
+		<div class="pic headshot round" style="background:url(images/organizers/elisa.png);background-size:100px 100px;"></div>
 		<div class="info-container">
-			<div class="name">Chonggang Wang</div>
-			<div class="role bold">IEEE Fellow, Founding EiC of IEEE IoT Journal, EiC of IEEE Network</div>
-			<div class="affiliation">InterDigital, Inc., USA</div>
+			<div class="name">Elisa Bertino</div>
+			<!-- <div class="role bold">IEEE Fellow, ACM Fellow</div> -->
+			<div class="affiliation">Purdue University, USA</div>
 		</div>
 	</div>
 	
 	<div class="flex flex-auto person-container">
-		<div class="pic headshot round" style="background:url(images/organizers/elisa.png);background-size:100px 100px;"></div>
+		<div class="pic headshot round" style="background:url(images/organizers/cuishuguang.png);background-size:100px 100px;"></div>
 		<div class="info-container">
-			<div class="name">Elisa Bertino</div>
-			<div class="role bold">IEEE Fellow, ACM Fellow</div>
-			<div class="affiliation">Purdue University, USA</div>
+			<div class="name">Shuguang Cui</div>
+			<!-- <div class="role bold">IEEE Fellow</div> -->
+			<div class="affiliation">The Chinese University of Hong Kong, Shenzhen, China</div>
 		</div>
 	</div>
 
@@ -45,8 +45,17 @@ require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
 		<div class="pic headshot round" style="background:url(images/organizers/renkui.png);background-size:100px 100px;"></div>
 		<div class="info-container">
 			<div class="name">Kui Ren</div>
-			<div class="role bold">IEEE Fellow, ACM Fellow</div>
+			<!-- <div class="role bold">IEEE Fellow, ACM Fellow</div> -->
 			<div class="affiliation">Zhejiang University, China</div>
+		</div>
+	</div>
+
+	<div class="flex flex-auto person-container">
+		<div class="pic headshot round" style="background:url(images/organizers/chonggangwang.png);background-size:100px 100px;"></div>
+		<div class="info-container">
+			<div class="name">Chonggang Wang</div>
+			<!-- <div class="role bold">IEEE Fellow, Founding EiC of IEEE IoT Journal, EiC of IEEE Network</div> -->
+			<div class="affiliation">InterDigital, Inc., USA</div>
 		</div>
 	</div>
 
@@ -54,19 +63,11 @@ require 'header-left-bar.php'; //THIS MUST BE INCLUDED FOR EACH PAGE
 		<div class="pic headshot round" style="background:url(images/organizers/leizhang.png);background-size:100px 100px;"></div>
 		<div class="info-container">
 			<div class="name">Lei Zhang</div>
-			<div class="role bold">Associate Editor of IEEE IoT Journal, IEEE Wireless Communications Letters and Digital Communications and Networks</div>
+			<!-- <div class="role bold">Associate Editor of IEEE IoT Journal, IEEE Wireless Communications Letters and Digital Communications and Networks</div> -->
 			<div class="affiliation">University of Glasgow, UK</div>
 		</div>
 	</div>
 
-	<div class="flex flex-auto person-container">
-		<div class="pic headshot round" style="background:url(images/organizers/cuishuguang.png);background-size:100px 100px;"></div>
-		<div class="info-container">
-			<div class="name">Shuguang Cui</div>
-			<div class="role bold">IEEE Fellow</div>
-			<div class="affiliation">The Chinese University of Hong Kong, Shenzhen, China</div>
-		</div>
-	</div>
 	
 	<div class="spacer"></div>
 	
